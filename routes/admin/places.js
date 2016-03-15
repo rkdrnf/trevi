750c4a589c1d96f45e771be83e537a80a3177fd3
Junsang <junsangs2@naver.com>
2016-03-16 01:35:53 +0900

    implementing places and initMap

--- routes/admin/places.js
+++ routes/admin/places.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var Place = require('../../models/place.js');
+
+router.get('/', function(req, res) {
+	Place.find().lean().exec(function(err, places) {
+		res.render('admin/places/index', { places: places });
+	});
+});
 router.get('/new', function(req, res, next) {
 		res.render('admin/places/new');
 });
