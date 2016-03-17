d924beddf07e6a3be8d07f41e062685864d9585b
rkdrnf <rkdrnf@gmail.com>
2016-03-17 18:57:18 +0900

    reduced minlength for title

--- models/article.js
+++ models/article.js
@@ -6,7 +6,7 @@ var article = new Schema({
 	author: { type: Schema.Types.ObjectId, ref: 'User', required: true }, 					//저자
 	regions: [{ type: Schema.Types.ObjectId, ref: 'Region' }],											//속한 지역
 	board: { type: Schema.Types.ObjectId, ref: 'Board' },														//속한 지역에 무슨 게시판인지
-	title: { type: String, minlength: 4, maxlength: 40, required: true },						//제목
+	title: { type: String, minlength: 2, maxlength: 40, required: true },						//제목
 	content: { type: String, required: true }, 																			//내용
 	photos: [{ type: Schema.Types.ObjectId, ref: 'Photo' }],
 	comments: [Comment.schema]
