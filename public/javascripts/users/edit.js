94407c7769c95aaebc49eb487bad71d42f5bfde1
Junsang <junsangs2@naver.com>
2016-04-20 23:28:23 +0900

    user profile page modified

--- public/javascripts/users/edit.js
+++ public/javascripts/users/edit.js
@@ -1,6 +1,12 @@
 $(function() {
 	var birth = $('#birthValue').val().split('-');
 
+	for(var y = 1998; y >= 1916; y--) {
+		$('#userEditForm select[name="birth_year"]').append(
+			$('<option value="' + y + '">' + y + '</option>')
+		);
+	}
+
 	for(var i = 1; i <= 12; i++) {
 		$('#userEditForm select[name="birth_month"]').append(
 			$('<option value="' + i + '">' + i + '월</option>')
@@ -13,18 +19,12 @@ $(function() {
 		);
 	}
 
-	for(var y = 1998; y >= 1916; y--) {
-		$('#userEditForm select[name="birth_year"]').append(
-			$('<option value="' + y + '">' + y + '</option>')
-		);
-	}
-
 	console.log(birth);
 
 	if (birth.length === 3) {
-		$('#userEditForm select[name="birth_month"]').val(+birth[0] + 1);
-		$('#userEditForm select[name="birth_date"]').val(birth[1]);
-		$('#userEditForm select[name="birth_year"]').val(birth[2]);
+		$('#userEditForm select[name="birth_year"]').val(birth[0]);
+		$('#userEditForm select[name="birth_month"]').val(+birth[1] + 1);
+		$('#userEditForm select[name="birth_date"]').val(birth[2]);
 	}
 
 	function handleFileSelect(evt) {
